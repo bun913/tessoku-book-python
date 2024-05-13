9729bf875b9e01cae27006522fa1cb996568ff18
bun913 <nagasima.romance+@gmail.com>
2024-05-13 18:59:31 +0900

    初期化

---
+++ b65/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+解く前のメモ用
+"""
+from sys import setrecursionlimit
+
+setrecursionlimit(10**8)
+
+
+def solve():
+    arrange()
+    act()
+
+
+def arrange():
+    pass
+
+
+def act():
+    pass
+
+solve()
