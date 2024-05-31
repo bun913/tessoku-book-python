8976ccfdca849955c9189bffbe5cf6b5c640c061
bun913 <nagasima.romance+@gmail.com>
2024-06-01 05:40:03 +0900

    feat add a06

--- a06/main.py
+++ a06/main.py
@@ -8,15 +8,30 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, Q, A, S = arrange()
+    act(N, Q, S)
 
 
 def arrange():
-    pass
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+    S = [0 for _ in range(N)]
+    for i in range(N):
+        a = A[i]
+        if i == 0:
+            S[i] = a
+        else:
+            S[i] = S[i-1] + a
+    return N, Q, A, S
 
 
-def act():
-    pass
+def act(N, Q, S):
+    for q in range(Q):
+        l, r = map(int, input().split())
+        if l == 1:
+            print(S[r-1])
+            continue
+        print(S[r-1] - S[l-2])
+
 
 solve()
