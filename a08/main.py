1e7dbe176172a2a71ef9f8c8c69c8e7ad17e69d6
bun913 <nagasima.romance+@gmail.com>
2024-06-03 06:04:39 +0900

    feat: add a08

--- a08/main.py
+++ a08/main.py
@@ -8,15 +8,33 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    H, W, xy, Q, accum = arrange()
+    act(xy, Q, accum)
 
 
 def arrange():
-    pass
+    H, W = map(int, input().split())
+    xy = [list(map(int, input().split())) for _ in range(H)]
+    Q = int(input())
+    # 二次元の累積和用の表を作る
+    # 表の長さ、高さはそれぞれH+1, W+1としておく
+    accum = [[0] * (W+1) for _ in range(H+1)]
+    # まずは横方向に累積和を取る
+    for i in range(1, H+1):
+        for j in range(1, W+1):
+            accum[i][j] = accum[i][j-1] + xy[i-1][j-1]
+    # 縦方向に累積和を取る
+    for j in range(1, W+1):
+        for i in range(H+1):
+            accum[i][j] += accum[i-1][j]
+    return H, W, xy, Q, accum
 
 
-def act():
-    pass
+def act(xy, Q, accum):
+    for q in range(Q):
+        a, b, c, d = map(int, input().split())
+        total = accum[c][d] - accum[a-1][d] - accum[c][b-1] + accum[a-1][b-1]
+        print(total)
+
 
 solve()
