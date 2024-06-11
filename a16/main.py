e4eb961a215028c431d07b5ba82cf0a0be93c1b4
bun913 <nagasima.romance+@gmail.com>
2024-06-12 07:24:01 +0900

    feat: add a16

--- a16/main.py
+++ a16/main.py
@@ -8,15 +8,27 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A, B = arrange()
+    act(N, A, B)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    return N, A, B
 
 
-def act():
-    pass
+def act(N, A, B):
+    dp = [0] * N
+    for i in range(1, N):
+        if i == 1:
+            dp[i] = A[0]
+            continue
+        cand1 = dp[i-1] + A[i-1]
+        cand2 = dp[i-2] + B[i-2]
+        dp[i] = min(cand1, cand2)
+    print(dp[-1])
+
 
 solve()
