5c47cab90a0aa0bd41947b5f109e9150d83c1bd0
bun913 <nagasima.romance+@gmail.com>
2024-06-15 06:33:40 +0900

    feat: add a19

--- a19/main.py
+++ a19/main.py
@@ -8,15 +8,32 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, W, wv = arrange()
+    dp = makedp(N, W, wv)
+    act(dp)
 
 
 def arrange():
-    pass
+    N, W = map(int, input().split())
+    wv = [list(map(int, input().split())) for _ in range(N)]
+    return N, W, wv
 
 
-def act():
-    pass
+def makedp(N, W, wv):
+    dp = [[0 for _ in range(W+1)] for _ in range(N+1)]
+    for i in range(1, N+1):
+        w, v = wv[i-1]
+        for j in range(W+1):
+            if j < w:
+                dp[i][j] = dp[i-1][j]
+                continue
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+    return dp
+
+
+def act(dp):
+    print(dp[-1][-1])
+
 
 solve()
