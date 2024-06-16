f901aca4919bef0d2482d87240cbe7eb90871bc4
bun913 <nagasima.romance+@gmail.com>
2024-06-17 07:19:16 +0900

    feat: add a22

--- a22/main.py
+++ a22/main.py
@@ -8,15 +8,24 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A, B = arrange()
+    act(N, A, B)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    return N, A, B
 
 
-def act():
-    pass
+def act(N, A, B):
+    dp = [-(10**9)] * (N + 1)
+    dp[1] = 0
+    for i in range(1, N):
+        dp[A[i-1]] = max(dp[A[i-1]], dp[i]+100)
+        dp[B[i-1]] = max(dp[B[i-1]], dp[i]+150)
+    print(dp[N])
+
 
 solve()
