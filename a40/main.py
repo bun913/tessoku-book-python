7bbc494de028fbcb4fe68d5fc7ec702a36697ed8
bun913 <nagasima.romance+@gmail.com>
2024-07-02 06:03:46 +0900

    feat: a40

--- a40/main.py
+++ a40/main.py
@@ -3,20 +3,53 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from operator import mul
+from functools import reduce
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A = arrange()
+    act(N, A)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    return N, A
 
 
-def act():
-    pass
+def act(N, A):
+    memo = enumrate(A)
+    ans = 0
+    for k, v in memo.items():
+        if v < 3:
+            continue
+        cnt = cmb(v, 3)
+        ans += cnt
+    print(ans)
+
+
+def enumrate(A):
+    dic = {}
+    for a in A:
+        if a in dic:
+            dic[a] += 1
+        else:
+            dic[a] = 1
+    return dic
+
+
+def cmb(n, r):
+    r = min(n-r, r)
+    if r == 0:
+        return 1
+    elif r < 0:
+        return 0
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
+
 
 solve()
