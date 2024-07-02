c0f8a8b37691b82b333b9f27844fbd589b92d43a
bun913 <nagasima.romance+@gmail.com>
2024-07-03 06:34:21 +0900

    feat: a41

--- a41/main.py
+++ a41/main.py
@@ -8,15 +8,27 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, S = arrange()
+    act(N, S)
 
 
 def arrange():
-    pass
+    N = int(input())
+    S = input()
+    return N, S
 
 
-def act():
-    pass
+def act(N,  S):
+    # 最後の手から考える
+    # 最後に3色連続している部分があれば絶対に達成できる
+    for i in range(N-2):
+        s1 = S[i]
+        s2 = S[i+1]
+        s3 = S[i+2]
+        if s1 == s2 and s2 == s3:
+            print("Yes")
+            exit()
+    print("No")
+
 
 solve()
