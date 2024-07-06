247e84cfc01c35b6f90cf743e6642ef7c30cf2f8
bun913 <nagasima.romance+@gmail.com>
2024-07-07 06:44:22 +0900

    feat: a51

--- a51/main.py
+++ a51/main.py
@@ -3,20 +3,31 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from collections import deque
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    Q = arrange()
+    act(Q)
 
 
 def arrange():
-    pass
+    return int(input())
 
 
-def act():
-    pass
+def act(Q):
+    q = deque([])
+    for _ in range(Q):
+        s = input()
+        if s.startswith("1"):
+            _, book = s.split(" ")
+            q.appendleft(book)
+        elif s == "2":
+            print(q[0])
+        else:
+            q.popleft()
+
 
 solve()
