50d98ba504e5c817238fff25617b93bd35ac31d0
bun913 <nagasima.romance+@gmail.com>
2024-07-09 06:11:30 +0900

    feat: a53

--- a53/main.py
+++ a53/main.py
@@ -3,20 +3,35 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+import heapq
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    Q = arrange()
+    act(Q)
 
 
 def arrange():
-    pass
+    return int(input())
 
 
-def act():
-    pass
+def act(Q):
+    q = []
+    heapq.heapify(q)
+    for _ in range(Q):
+        s = input()
+        if s.startswith("1"):
+            _, x = map(int, s.split())
+            heapq.heappush(q, x)
+            continue
+        if s == "2":
+            num = heapq.heappop(q)
+            print(num)
+            heapq.heappush(q, num)
+            continue
+        heapq.heappop(q)
+
 
 solve()
