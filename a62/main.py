7311b2e629f4bc908bf195e4963272028986a136
bun913 <nagasima.romance+@gmail.com>
2024-07-15 09:32:05 +0900

    feat:a62

--- a62/main.py
+++ a62/main.py
@@ -3,20 +3,41 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from collections import deque
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, M = arrange()
+    act(N, M)
 
 
 def arrange():
-    pass
+    return map(int, input().split())
 
 
-def act():
-    pass
+def act(N, M):
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    for i in range(1, N+1):
+        q = deque([i])
+        visited = [False] * (N+1)
+        while q:
+            cur = q.popleft()
+            visited[cur] = True
+            for nex in graph[cur]:
+                if visited[nex] is True:
+                    continue
+                q.appendleft(nex)
+        if False in visited[1:]:
+            print("The graph is not connected.")
+            exit()
+        print("The graph is connected.")
+        exit()
+
 
 solve()
