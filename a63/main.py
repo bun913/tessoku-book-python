be300d0a02e572fa37113ec107862d88aadf8f37
bun913 <nagasima.romance+@gmail.com>
2024-07-16 06:26:00 +0900

    feat: a63

--- a63/main.py
+++ a63/main.py
@@ -3,20 +3,40 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from collections import deque
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, M = arrange()
+    act(N, M)
 
 
 def arrange():
-    pass
+    return map(int, input().split())
 
 
-def act():
-    pass
+def act(N, M):
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    # 頂点1から頂点kまでの最短距離を保存する
+    dis = [-1 for _ in range(N+1)]
+    dis[0] = 0
+    dis[1] = 0
+    q = deque([1])
+    while q:
+        cur = q.pop()
+        for nex in graph[cur]:
+            if dis[nex] != -1:
+                continue
+            dis[nex] = dis[cur] + 1
+            q.appendleft(nex)
+    for i in range(1, N+1):
+        print(dis[i])
+
 
 solve()
