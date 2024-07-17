95d84ddaa23313bd87020409e45a0f4f206abb03
bun913 <nagasima.romance+@gmail.com>
2024-07-18 06:21:08 +0900

    feat: a65

--- a65/main.py
+++ a65/main.py
@@ -8,15 +8,33 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A = arrange()
+    act(N, A)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = [0] * 2 + list(map(int, input().split()))
+    return N, A
 
 
-def act():
-    pass
+def act(N, A):
+    graph = make_graph(N, A)
+    # 部下の数をメモする動的計画法の配列
+    dp = [0 for _ in range(N+1)]
+    for i in range(N, 0, -1):
+        for buka in graph[i]:
+            dp[i] += dp[buka] + 1
+    print(*dp[1:])
+
+
+def make_graph(N, A):
+    graph = [[] for _ in range(N+1)]
+    for i in range(2, N+1):
+        # 上司から部下の方向にグラフを貼る
+        a = A[i]
+        graph[a].append(i)
+    return graph
+
 
 solve()
