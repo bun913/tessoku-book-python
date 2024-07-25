a36037a0d8a81d875d475491ca8ac6e3c7ab54b4
bun913 <nagasima.romance+@gmail.com>
2024-07-26 07:33:53 +0900

    feat/ b03

--- b03/main.py
+++ b03/main.py
@@ -8,15 +8,27 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A = arrange()
+    act(N, A)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    return N, A
 
 
-def act():
-    pass
+def act(N, A):
+    for i in range(N):
+        for j in range(i + 1, N):
+            for k in range(j + 1, N):
+                a = A[i]
+                b = A[j]
+                c = A[k]
+                if a + b + c == 1000:
+                    print("Yes")
+                    exit()
+    print("No")
+
 
 solve()
