edbb4812ed364d795a9071fbe82ab4c3bb5cae01
bun913 <nagasima.romance+@gmail.com>
2024-07-30 06:15:45 +0900

    feat: b07

--- b07/main.py
+++ b07/main.py
@@ -8,15 +8,33 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    T, N, deltas = arrange()
+    act(T, N, deltas)
 
 
 def arrange():
-    pass
+    T = int(input())
+    N = int(input())
+    deltas = [0 for _ in range(T)]
+    return T, N, deltas
 
 
-def act():
-    pass
+def act(T, N, deltas):
+    for i in range(N):
+        l, r = map(int, input().split())
+        deltas[l] += 1
+        if r < len(deltas):
+            deltas[r] -= 1
+    # 累積和を出す
+    timings = [0 for _ in range(T)]
+    for i in range(T):
+        if i == 0:
+            timings[i] = deltas[i]
+        else:
+            timings[i] = deltas[i] + timings[i-1]
+    # 結果を出力する
+    for t in timings:
+        print(t)
+
 
 solve()
