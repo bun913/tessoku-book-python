14a3a82abacd102bd20ae9be933d1e819179beb2
bun913 <nagasima.romance+@gmail.com>
2024-08-03 08:14:55 +0900

    feat: b12

--- b12/main.py
+++ b12/main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 解く前のメモ用
+xはたかだか10**2も行かない
+早退誤差または絶対誤差が0.001以下であれば正解となる
 """
 from sys import setrecursionlimit
 
@@ -8,15 +10,34 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N = arrange()
+    act(N)
 
 
 def arrange():
-    pass
+    return int(input())
 
 
-def act():
-    pass
+def act(N):
+    ok = 100.0
+    ng = 0
+    ans = meguru_bisect(ng, ok, N)
+    print(ans)
+
+
+def meguru_bisect(ng, ok, N):
+    while (ok-ng > 0.001):
+        mid = (ok+ng)/2
+        if is_ok(mid, N):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+def is_ok(x, N):
+    if x * x * x + x >= N:
+        return True
+
 
 solve()
