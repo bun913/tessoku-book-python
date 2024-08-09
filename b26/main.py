70c6b82df5cdd1d420e8248a63fe0f7a93700651
bun913 <nagasima.romance+@gmail.com>
2024-08-10 07:41:09 +0900

    feat: b26

--- b26/main.py
+++ b26/main.py
@@ -8,15 +8,34 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N = arrange()
+    act(N)
 
 
 def arrange():
-    pass
+    N = int(input())
+    return N
 
 
-def act():
-    pass
+def act(N):
+    primse = makeprimes(N)
+    for i in range(N+1):
+        if primse[i]:
+            print(i)
+
+
+def makeprimes(N):
+    primse = [True for _ in range(N+9)]
+    primse[0] = False
+    primse[1] = False
+    for n in range(2, N+1):
+        if primse[n] is False:
+            continue
+        q = n * 2
+        while q <= N:
+            primse[q] = False
+            q += n
+    return primse
+
 
 solve()
