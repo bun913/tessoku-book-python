7166b3634082af0619c9950c7721409193e1b848
bun913 <nagasima.romance+@gmail.com>
2024-08-12 10:32:53 +0900

    feat: b28

--- b28/main.py
+++ b28/main.py
@@ -6,17 +6,31 @@ from sys import setrecursionlimit
 
 setrecursionlimit(10**8)
 
+MOD = 10**9+7
+
 
 def solve():
-    arrange()
-    act()
+    N = arrange()
+    fibos = makefib(N)
+    act(N, fibos)
 
 
 def arrange():
+    return int(input())
     pass
 
 
-def act():
-    pass
+def makefib(N):
+    fibos = [0 for _ in range(N+9)]
+    fibos[1] = 1
+    fibos[2] = 1
+    for i in range(3, N+1):
+        fibos[i] = (fibos[i-1] + fibos[i-2]) % MOD
+    return fibos
+
+
+def act(N, fibos):
+    print(fibos[N])
+
 
 solve()
