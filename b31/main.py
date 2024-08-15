69f449e2e460b95c368716bc9bd003f30f397f80
bun913 <nagasima.romance+@gmail.com>
2024-08-15 12:25:48 +0900

    feat: b31

--- b31/main.py
+++ b31/main.py
@@ -8,15 +8,24 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N = arrange()
+    act(N)
 
 
 def arrange():
-    pass
+    return int(input())
 
 
-def act():
-    pass
+def act(N):
+    n3 = N // 3
+    n5 = N // 5
+    n7 = N // 7
+    n15 = N // 15
+    n21 = N // 21
+    n35 = N // 35
+    n105 = N // 105
+    ans = n3 + n5 + n7 - n15 - n21 - n35 + n105
+    print(ans)
+
 
 solve()
