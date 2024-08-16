f87044cb57e38514d1b6657e8d64e4dc8d9fe473
bun913 <nagasima.romance+@gmail.com>
2024-08-16 20:29:31 +0900

    feat: b32

--- b32/main.py
+++ b32/main.py
@@ -8,15 +8,27 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, K, A = arrange()
+    act(N, K, A)
 
 
 def arrange():
-    pass
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    return N, K, A
 
 
-def act():
-    pass
+def act(N, K, A):
+    is_wins = [False] * (N+1)
+    for n in range(1, N+1):
+        for a in A:
+            if n-a >= 0 and is_wins[n-a] is False:
+                is_wins[n] = True
+                break
+    if is_wins[N] is True:
+        print("First")
+        exit()
+    print("Second")
+
 
 solve()
