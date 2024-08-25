795cf10b9afd81839ef8712e487985f3711e77fb
bun913 <nagasima.romance+@gmail.com>
2024-08-26 06:15:00 +0900

    feat: b41

--- b41/main.py
+++ b41/main.py
@@ -8,15 +8,31 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    X, Y = arrange()
+    act(X, Y)
 
 
 def arrange():
-    pass
+    return map(int, input().split())
 
 
-def act():
-    pass
+def act(X, Y):
+    # 1同士なら何もしなくてもOK
+    if X == 1 and Y == 1:
+        print(0)
+        exit()
+    # それ以外はXとYの最大公約数は1という制約がある
+    ans = []
+    x, y = X, Y
+    while x >= 2 or y >= 2:
+        ans.append((x, y))
+        if x > y:
+            x -= y
+        else:
+            y -= x
+    print(len(ans))
+    for x, y in reversed(ans):
+        print(x, y)
+
 
 solve()
