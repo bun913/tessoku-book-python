f09b5dade73beee20efb4bcd682914a3eeff057d
bun913 <nagasima.romance+@gmail.com>
2024-08-27 06:13:51 +0900

    feat: b44

--- b44/main.py
+++ b44/main.py
@@ -8,15 +8,28 @@ setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, A = arrange()
+    act(N, A)
 
 
 def arrange():
-    pass
+    N = int(input())
+    A = [list(map(int, input().split())) for _ in range(N)]
+    return N, A
 
 
-def act():
-    pass
+def act(N, A):
+    # i行目の行が今どの行に交換されているかを管理する
+    row_cnts = [i for i in range(N)]
+    Q = int(input())
+    for _ in range(Q):
+        q, x, y = map(int, input().split())
+        if q == 1:
+            row_cnts[x-1], row_cnts[y-1] = row_cnts[y-1], row_cnts[x-1]
+            continue
+        # 取得操作
+        a = row_cnts[x-1]
+        print(A[a][y-1])
+
 
 solve()
