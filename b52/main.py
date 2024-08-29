dbdb153ac1a2083d5aec420043400d3ca3d3d77d
bun913 <nagasima.romance+@gmail.com>
2024-08-30 06:26:33 +0900

    feat: b52

--- b52/main.py
+++ b52/main.py
@@ -3,20 +3,38 @@
 解く前のメモ用
 """
 from sys import setrecursionlimit
+from collections import deque
 
 setrecursionlimit(10**8)
 
 
 def solve():
-    arrange()
-    act()
+    N, X, A = arrange()
+    act(N, X, A)
 
 
 def arrange():
-    pass
+    N, X = map(int, input().split())
+    A = list(input())
+    return N, X, A
 
 
-def act():
-    pass
+def act(N, X, A):
+    # 操作1 キューに入れてX番目のボールを青くする
+    X -= 1
+    q = deque([X])
+    q.append(X)
+    A[X] = "@"
+    while q:
+        # キューの先頭を取り出す
+        pos = q.popleft()
+        if pos-1 >= 0 and A[pos-1] == ".":
+            A[pos-1] = "@"
+            q.appendleft(pos-1)
+        if pos + 1 < N and A[pos+1] == ".":
+            A[pos+1] = "@"
+            q.appendleft(pos+1)
+    print(*A, sep="")
+
 
 solve()
